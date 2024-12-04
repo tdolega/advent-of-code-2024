9d7436a1f3be8914d9322cea231afe8a34463bb6
tdolega <tymoteuszdolega@gmail.com>
2024-12-04 08:29:24 +0100

    1

---
+++ 1/solution.py
@@ -0,0 +1,11 @@
+with open("input1.txt") as f:
+    rows = [list(map(int, line.split())) for line in f]
+    columns = list(zip(*rows))
+    lcol, rcol = columns
+lcol, rcol = sorted(lcol), sorted(rcol)
+distance_sum = sum(abs(l - r) for l, r in zip(lcol, rcol))
+print(f"solution 1: {distance_sum}")
+
+n_l_in_r = map(rcol.count, lcol)
+similarity_score = sum(l * r for l, r in zip(lcol, n_l_in_r))
+print(f"solution 2: {similarity_score}")
